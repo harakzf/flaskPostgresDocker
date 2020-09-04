4f12e886170ff5b12a5eacd478d1afdaf9350c6f
harakzf <14cc123f@al.rikkyo.ac.jp>
2020-09-04 10:45:39 +0900

    mody1

--- update.sh
+++ update.sh
@@ -1,15 +1,13 @@
+declare time=`date '+%T'`
+mkdir ./gitLog/${time}
+
 declare commitWord=$1
-declare date=`date '+%T'`
-
-mkdir ${date}_log
-
-declare GIT_COMMIT_LOG=./${date}_log/gitCommit.log
 
 # ステージ追加
 git add --all . 
 
 # コミット
-git commit -m "${commitWord}" > $GIT_COMMIT_LOG
+git commit -m "${commitWord}" > ./gitLog/${time}/gitCommit.log
 
 # プッシュ
 git push origin master
